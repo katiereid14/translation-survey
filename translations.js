bf6f01e834bc11b22e911303b050b2d3213b015c
katiereid14 <kreid@bowdoin.edu>
2022-11-17 17:04:42 -0500

    start of if loop

--- translations.js
+++ translations.js
@@ -1,213 +1,290 @@
 var translations = [
   {
+    "sentence_number": 1,
     "sentences": "You drop a bowling ball on a porcelain figure <br> T&uacute tiras una bola de boliche a una figura de porcelana"
   },
   {
-    "sentences": "You drop a bowling ball on a piece of cake <br> T&uacute tiras una bola de boliche en un pedazo de pastel"
-  },
-
-  {
-    "sentences": "You drop a bowling ball on an empty can of beer <br> T&uacute tiras una bola de boliche en una lata de cerveza vac&iacutea"
+    "sentence_number": 2,
+    "sentences": "You drop a bowling ball on a piece of cake <br> T&uacute tiras una bola de boliche a un trozo de pastel"
   },
   {
+    "sentence_number": 3,
+    "sentences": "You drop a bowling ball on an empty can of beer <br> T&uacute tiras una bola de boliche a una lata de cerveza vac&iacutea"
+  },
+  {
+    "sentence_number": 4,
     "sentences": "You drop a bowling ball on a cracker <br> T&uacute tiras una bola de boliche a una galleta"
   },
   {
-    "sentences": "You drop a bowling ball on a wine glass <br> T&uacute tiras una bola de boliche a un vaso de vino"
+    "sentence_number": 5,
+    "sentences": "You drop a bowling ball on a wine glass <br> T&uacute tiras una bola de boliche a una copa de vino"
   },
   {
+    "sentence_number": 6,
     "sentences": "You drop a bowling ball on an egg <br> T&uacute tiras una bola de boliche a un huevo"
   },
   {
+    "sentence_number": 7,
     "sentences": "You drop a bowling ball on an egg flan <br> T&uacute tiras una bola de boliche a un flan"
   },
   {
-    "sentences": "You drop a bowling ball on a blackberry <br> T&uacute tiras una bola de boliche a una morra"
+    "sentence_number": 8,
+    "sentences": "You drop a bowling ball on a blackberry <br> T&uacute tiras una bola de boliche a una mora"
   },
   {
+    "sentence_number": 9,
     "sentences": "You drop a bowling ball on glasses <br> T&uacute tiras una bola de boliche a unas gafas"
   },
   {
+    "sentence_number": 10,
     "sentences": "You drop a bowling ball on a hat <br> T&uacute tiras una bola de boliche a una gorra"
   },
   {
+    "sentence_number": 11,
     "sentences": "You drop a bowling ball on a chocolate ice-cream <br> T&uacute tiras una bola de boliche a un helado de chocolate"
   },
   {
+    "sentence_number": 12,
     "sentences": "You drop a bowling ball on an iPAD <br> T&uacute tiras una bola de boliche a un iPad"
   },
   {
+    "sentence_number": 13,
     "sentences": "You drop a bowling ball on an iPhone <br> T&uacute tiras una bola de boliche a un iPhone"
   },
   {
-    "sentences": "You drop a bowling ball on a light bulb <br> T&uacute tiras una bola de boliche a un foco"
+    "sentence_number": 14,
+    "sentences": "You drop a bowling ball on a light bulb <br> T&uacute tiras una bola de boliche a una bombilla"
   },
   {
+    "sentence_number": 15,
     "sentences": "You drop a bowling ball on a mirror <br> T&uacute tiras una bola de boliche a un espejo"
   },
   {
-    "sentences": "You drop a bowling ball on a chocolate muffin <br> T&uacute tiras una bola de boliche a un muffin de chocolate"
+    "sentence_number": 16,
+    "sentences": "You drop a bowling ball on a chocolate muffin <br> T&uacute tiras una bola de boliche a una magdalena de chocolate"
   },
   {
+    "sentence_number": 17,
     "sentences": "You drop a bowling ball on a donut <br> T&uacute tiras una bola de boliche a una rosquilla"
   },
   {
-    "sentences": "You drop a bowling ball on a rice cake <br> T&uacute tiras una bola de boliche pastel de arroz"
+    "sentence_number": 18,
+    "sentences": "You drop a bowling ball on a rice cake <br> T&uacute tiras una bola de boliche a una tortita de arroz"
   },
   {
+    "sentence_number": 19,
     "sentences": "You drop a bowling ball on a ceramic plate <br> T&uacute tiras una bola de boliche plato de cer&aacutemica"
   },
   {
+    "sentence_number": 20,
     "sentences": "You drop a bowling ball on a strawberry <br> T&uacute tiras una bola de boliche a una fresa"
   },
   {
+    "sentence_number": 21,
     "sentences": "You drop a bowling ball on a piece of sushi <br> T&uacute tiras una bola de boliche a un pedazo de sushi"
   },
   {
-    "sentences": "You drop a bowling ball on a tile <br> T&uacute tiras una bola de boliche a una teja"
+    "sentence_number": 22,
+    "sentences": "You drop a bowling ball on a tile <br> T&uacute tiras una bola de boliche a una baldosa"
   },
   {
+    "sentence_number": 23,
     "sentences": "You drop a bowling ball on a tomato <br> T&uacute tiras una bola de boliche a un tomate"
   },
   {
-    "sentences": "You drop a bowling ball on a paper cup <br> T&uacute tiras una bola de boliche a una taza de papel"
+    "sentence_number": 24,
+    "sentences": "You drop a bowling ball on a paper cup <br> T&uacute tiras una bola de boliche a un vaso de papel"
   },
   {
-    "sentences": "You see how a musician is playing a guitar <br> T&uacute ves como un m&uacutesico toca la guitarra."
+    "sentence_number": 25,
+    "sentences": "You drop a toothbrush on the sponge <br> T&uacute tiras un cepillo de dientes en la esponja"
   },
   {
-    "sentences": "You see how a cat is drinking water from a bowl <br> T&uacute ves como un gato bebe agua de un cuenco."
+    "sentence_number": 26,
+    "sentences": "You drop a fork on the plate <br> T&uacute tiras un tenedor al plato"
   },
   {
-    "sentences": "You see how an old man is hammering a nail <br> T&uacute ves como un hombre anciano martilla un clavo."
+    "sentence_number": 27,
+    "sentences": "You see how a musician is playing a guitar <br> T&uacute ves como un m&uacutesico est&aacute tocando la guitarra"
   },
   {
-    "sentences": "You see how a boy is peeling a banana <br> T&uacute ves como un ni&ntildeo pela una banana"
+    "sentence_number": 28,
+    "sentences": "You see how a cat is drinking water from a bowl <br> T&uacute ves como un gato est&aacute bebiendo agua de un cuenco"
   },
   {
-    "sentences": "You see how an elephant is eating a watermelon <br> T&uacute ves como un elefante come una sand&iacutea"
+    "sentence_number": 29,
+    "sentences": "You see how an old man is hammering a nail <br> T&uacute ves como un hombre anciano est&aacute martillando un clavo"
   },
   {
-    "sentences": "You see how a postman is driving a car <br> T&uacute ves como un cartero maneja un carro"
+    "sentence_number": 30,
+    "sentences": "You see how a boy is peeling a banana <br> T&uacute ves como un ni&ntildeo est&aacute pelando una banana"
   },
   {
-    "sentences": "You see how an astronaut is putting on a space suit <br> T&uacute ves como un astronauta se pone su traje espacial"
+    "sentence_number": 31,
+    "sentences": "You see how an elephant is eating a watermelon <br> T&uacute ves como un elefante est&aacute comiendo una sand&iacutea"
   },
   {
-    "sentences": "You see how a cook is taking a knife <br> T&uacute ves como un cocinero agarra un cuchillo"
+    "sentence_number": 32,
+    "sentences": "You see how a postman is driving a car <br> T&uacute ves como un cartero est&aacute conduciendo un coche"
   },
   {
-    "sentences": "You see how a photographer is taking a photo of a lion <br> T&uacute ves como un fot&oacutegrafo toma una foto de un le&oacuten."
+    "sentence_number": 33,
+    "sentences": "You see how an astronaut is putting on a space suit <br> T&uacute ves como un astronauta se est&aacute poniendo su traje espacial"
   },
   {
-    "sentences": "You see how a puppy is playing with a ball <br> T&uacute ves como un cachorro juega con una pelota."
+    "sentence_number": 34,
+    "sentences": "You see how a puppy is playing with a ball <br> T&uacute ves como un cachorro est&aacute jugando con una pelota"
   },
   {
-    "sentences": "You drop a cabbage on the table <br> T&uacute botas una col a la mesa"
+    "sentence_number": 35,
+    "sentences": "You drop a cabbage on the table <br> T&uacute tiras una col a la mesa"
   },
   {
-    "sentences": "You drop a keyboard on the bed <br> T&uacute botas un teclado a la cama"
+    "sentence_number": 36,
+    "sentences": "You drop a keyboard on the bed <br> T&uacute tiras un teclado a la cama"
   },
   {
-    "sentences": "You drop a watch on the laptop <br> T&uacute botas un reloj a una laptop"
+    "sentence_number": 37,
+    "sentences": "You drop a watch on the laptop <br> T&uacute tiras un reloj al port&aacutetil"
   },
   {
-    "sentences": "You drop a cushion on the magazine <br> T&uacute botas un coj&iacuten a una revista"
+    "sentence_number": 38,
+    "sentences": "You drop a cushion on the magazine <br> T&uacute tiras un coj&iacuten a la revista"
   },
   {
-    "sentences": "You drop a remote on the rug <br> T&uacute tiras un control remoto a una alfombra"
+    "sentence_number": 39,
+    "sentences": "You drop a remote on the rug <br> T&uacute tiras un control remoto a la alfombra"
   },
   {
-    "sentences": "You drop a pencil on the shoe <br> T&uacute tiras un l&acutepiz en el zapato."
+    "sentence_number": 40,
+    "sentences": "You drop a pencil on the shoe <br> T&uacute tiras un l&aacutepiz al zapato"
   },
   {
-    "sentences": "You drop a thermometer on the box <br> T&acute tiras un term&oacutemetro a una caja"
+    "sentence_number": 41,
+    "sentences": "You drop a ring on the newspaper <br> T&uacute tiras un anillo al peri&oacutedico"
   },
   {
-    "sentences": "You drop a ring on the newspaper <br> T&acute tiras un anillo a un periodico"
+    "sentence_number": 42,
+    "sentences": "You drop a needle on the mouse pad <br> T&uacute tiras una aguja a la alfombrilla para el rat&oacuten"
   },
   {
-    "sentences": "You drop a needle on the mouse pad <br> T&uacute tiras una aguja a un alfombrilla de raton"
+    "sentence_number": 43,
+    "sentences": "You see how a man is shaving his beard <br> T&uacute ves como un hombre se est&aacute afeitando la barba"
   },
   {
-    "sentences": "You see how a man is shaving his beard <br> T&uacute ves como un hombre se hace la barba"
+    "sentence_number": 44,
+    "sentences": "You see how a woman is taking money out of a wallet <br> T&uacute ves como una mujer est&aacute sacando dinero de su billetera"
   },
   {
-    "sentences": "You see how a woman is taking money out of a wallet <br> T&uacute ves como una mujer saca dinero de su cartera"
+    "sentence_number": 45,
+    "sentences": "You see how a donkey is drinking water <br> T&uacute ves como un burro est&aacute bebiendo agua"
   },
   {
-    "sentences": "You see how a donkey is drinking water <br> T&uacute ves como un burro bebe agua."
+    "sentence_number": 46,
+    "sentences": "You see how a man is boiling potatoes <br> T&uacute ves a un hombre est&aacute hirviendo papas"
   },
   {
-    "sentences": "You see how a man is boiling potatoes <br> T&uacute ves a un hombre hervir papas"
+    "sentence_number": 47,
+    "sentences": "You see how a dentist is drinking a can of cola <br> T&uacute ves como un dentista est&aacute bebiendo una lata de cola"
   },
   {
-    "sentences": "You see how a dentist is drinking a can of cola <br> T&uacute ves como un dentista bebe una botella de cola"
+    "sentence_number": 48,
+    "sentences": "You see how a nurse is taking a syringe <br> T&uacute ves como una enfermera est&aacute agarrando una jeringuilla"
   },
   {
-    "sentences": "You see how a nurse is taking a syringe <br> T&uacute ves como una enfermera agarra una jeringuilla"
+    "sentence_number": 49,
+    "sentences": "You see how a doctor is taking a stethoscope <br> T&uacute ves como un doctor est&aacute agarrando un estetoscopio"
   },
   {
-    "sentences": "You see how a doctor is taking a stethoscope <br> T&uacute ves como un doctor agarra un estetoscopio"
+    "sentence_number": 50,
+    "sentences": "You see how a student is writing on a board <br> T&uacute ves como un estudiante est&aacute escribiendo en la pizarra"
   },
   {
-    "sentences": "You see how a sTúdent is writing on a board <br> T&uacute ves como un est&uacutediante escribe en la pizarra"
+    "sentence_number": 51,
+    "sentences": "You see how an actress is taking a microphone <br> T&uacute ves como una actriz est&aacute agarrando el micr&oacutefono"
   },
   {
-    "sentences": "You see how an actress is taking a microphone <br> T&acute ves como un actris toma el microfono"
+    "sentence_number": 52,
+    "sentences": "You see how a kitten is playing in the sandbox <br> T&uacute ves como un gatito est&aacute jugando en la caja de arena"
   },
   {
-    "sentences": "You see how a kitten is playing in the sandbox <br> T&uacute ves como un gatito juega en la caja de arena"
+    "sentence_number": 53,
+    "sentences": "You drop a towel on the TV <br> T&uacute tiras una toalla a la televisi&oacuten"
   },
   {
-    "sentences": "You drop a towel on the TV <br> T&ucautetiras una toalla en la televisi&oacuten."
+    "sentence_number": 54,
+    "sentences": "You see how a teenage girl is playing the piano <br> T&uacute ves como una ni&ntildea adolescente est&aacute tocando el piano"
   },
   {
-    "sentences": "You see how a teenage girl is playing the piano <br> T&uacute ves como una ni&ntildea adolecente toca el piano"
+    "sentence_number": 55,
+    "sentences": "You see how a horse is chewing hay <br> T&uacute ves como un caballo est&aacute masticando el heno"
   },
   {
-    "sentences": "You see how a horse is chewing hay <br> T&uacute ves como un caballo mastica el heno"
+    "sentence_number": 56,
+    "sentences": "You see how a mouse is eating cheese <br> T&uacute ves como el rat&oacuten est&aacute comiendo queso"
   },
   {
-    "sentences": "You see how a mouse is eating cheese <br> T&uacute ves como el rat&oacuten come queso."
+    "sentence_number": 57,
+    "sentences": "You see how a panda is resting on a tree <br> T&uacute ves como un panda est&aacute descansando en un &aacuterbol"
   },
   {
-    "sentences": "You see how a panda is resting on a tree <br> T&uacute ves como un panda descansa en un &aacuterbol"
+    "sentence_number": 58,
+    "sentences": "You see how a parrot is eating an apple <br> T&uacute ves como un loro est&aacute comiendo una manzana"
   },
   {
-    "sentences": "You see how an owl is sitting on the branch <br> T&uacute ves como un b&uacuteho esta sentado en una rama"
+    "sentence_number": 59,
+    "sentences": "You see how a butterfly is sitting on a flower <br> T&uacute ves como una mariposa est&aacute reposando en una flor"
   },
   {
-    "sentences": "You see how a parrot is eating an apple <br> T&uacute ves como un perico come una manzana"
+    "sentence_number": 60,
+    "sentences": "You see how a hairdresser is taking scissors <br> T&uacute ves como una peluquera est&aacute agarrando las tijeras"
   },
   {
-    "sentences": "You see how a butterfly is sitting on a flower <br> T&uacute ves como una mariposa se siente en una flor."
+    "sentence_number": 61,
+    "sentences": "You see how a pilot is reading a book <br> T&uacute ves como un piloto est&aacute leyendo un libro"
   },
   {
-    "sentences": "You see how a hairdresser is taking scissors <br> T&uacute ves como una peluquera agarra las tijeras"
+    "sentence_number": 62,
+    "sentences": "You see how a police officer is talking on the phone <br> T&uacute ves como un polic&iacutea est&aacute hablando por el tel&eacutefono"
   },
   {
-    "sentences": "You see how a pilot is reading a book <br> T&uacute ves como un piloto lee un libro."
+    "sentence_number": 63,
+    "sentences": "You see how a president is writing in the notebook <br> T&uacute ves como un presidente est&aacute escribiendo en la libreta"
   },
   {
+    "sentence_number": 64,
+    "sentences": "You see how a woman is paying with a debit card <br> T&uacute ves como una mujer est&aacute pagando con una tarjeta de d&eacutebito"
+  },
+  {
+    "sentence_number": 65,
+    "sentences": "You see how a giraffe is playing with a zebra <br> T&uacute ves como una jirafa est&aacute jugando con una cebra"
+  },
+  {
+    "sentence_number": 66,
+    "sentences": "You see how a zebra is playing with a horse <br> T&uacute ves como una cebra est&aacute jugando con un caballo"
+  },
+  {
+    "sentence_number": 67,
+    "sentences": "You see how a man is playing with a cat <br> T&uacute ves como un hombre est&aacute jugando con un gato"
+  },
+  {
+    "sentence_number": 68,
     "sentences": "You see how a thief is stealing a wallet <br> T&uacute ves como un ladr&oacuten est&aacute robando una billetera"
   },
   {
-    "sentences": "You see how a police officer is talking on the phone <br> T&uacute ves como un polic&iacutea habla por el tel&eacutefono"
+    "sentence_number": 69,
+    "sentences": "You see how an owl is sitting on the branch <br> T&uacute ves como un b&uacuteho est&aacute reposando en una rama"
   },
   {
-    "sentences": "You see how a president is writing in the notebook <br> T&uacute ves como un presidente est&aacute escribiendo en el cuaderno"
+    "sentence_number": 70,
+    "sentences": "You drop a thermometer on the box <br> T&uacute tiras un term&oacutemetro a la caja"
   },
   {
-    "sentences": "You see how a woman is paying with a debit card <br> T&uacute ves como una mujer paga con una tarjeta de d&eacutebito"
+    "sentence_number": 71,
+    "sentences": "You see how a cook is taking a knife <br> T&uacute ves como un cocinero est&aacute agarrando un cuchillo"
   },
   {
-    "sentences": "You see how a giraffe is playing with a zebra <br> T&uacute ves como una jirafa juega con una cebra."
-  },
-  {
-    "sentences": "You see how a zebra is playing with a horse <br> T&uacute ves como una cebra juega con un caballo."
-  },
-  {
-    "sentences": "You see how a man is playing with a cat <br> T&uacute ves como un hombre juega con un gato."
+    "sentence_number": 72,
+    "sentences": "You see how a photographer is taking a photo of a lion <br> T&uacute ves como un fot&oacutegrafo est&aacute tomando una foto de un le&oacuten"
   }
 ]
